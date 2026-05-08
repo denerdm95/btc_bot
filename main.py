1ede93f6cec7731a0c44e2df3682d06ed7a730bf
denerdm95 <dener.dm@hotmail.com>
2026-05-07 22:39:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def home():
     return "Bot online"
 
 def run():
-    app.run(host='0.0.0.0', port=10000)
+    app.run(host='0.0.0.0', port=10000, threaded=True)
 
 def keep_alive():
     t = Thread(target=run)
